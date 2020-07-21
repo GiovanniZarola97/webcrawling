c97702987c9f307fa42c21322d0253fe01a76093
giovanni.zarola <giovanni.zarola@valuetech.it>
2020-07-21 14:21:57 +0200

    Updates

---
+++ precache-manifest.4b4a0677f021220f270ba6fa80fe9f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5906fe7be4df6c1ea276dc4012383f3",
+    "url": "/webcrawling/index.html"
+  },
+  {
+    "revision": "b0255c2f70ad16e82f8d",
+    "url": "/webcrawling/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2858073b859f599535e8",
+    "url": "/webcrawling/static/js/2.e800fcd9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/webcrawling/static/js/2.e800fcd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0255c2f70ad16e82f8d",
+    "url": "/webcrawling/static/js/main.b48fb771.chunk.js"
+  },
+  {
+    "revision": "509d3cb9a4b48c4192cf",
+    "url": "/webcrawling/static/js/runtime-main.032c5b67.js"
+  }
+]);
